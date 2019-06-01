f1de2aa656eec39c17f1d8e3b7d99b5a992bbcfa
Harris Yun <haryun@uw.edu>
2019-05-31 18:11:59 -0700

    initial commit moveable player, some boxes

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public int acceleration = 15;
+    public int maxSpeed = 7;
+    public int jumpForce = 400;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(new Vector2(Input.GetAxis("Horizontal") * acceleration, 0));
+        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), rb.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space)) {
+          rb.AddForce(Vector2.up * jumpForce);
+        }
+    }
+}
